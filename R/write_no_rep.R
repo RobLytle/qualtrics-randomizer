e795c4466b18e73744d1e186c5d9d322af9add08
Rob Lytle <47059344+RobLytle@users.noreply.github.com>
2021-07-01 13:08:25 -0400

    Update write_no_rep.R

--- R/write_no_rep.R
+++ R/write_no_rep.R
@@ -1,12 +1,6 @@
 library(tidyverse)
 
 
-test_df <- tibble(name = c("John Doe", "Jane Doe", "Alfred Hitchcock", "Thomas Edison"),
-								#	name_2 = c("John Doe", "Jane Doe", "Alfred Hitchcock", "Thomas Edison"),
-								#	y = c(1, 2, 3, 4)
-								)
-
-
 write_no_rep <- function(df, path){
 	
 	for (i in df$name){
@@ -24,19 +18,3 @@ write_no_rep <- function(df, path){
 
 write_no_rep(test_df, "data/names-test/func-test/")
 
-
-
-
-
-for (i in test_df$name){
-	#name_1 <- filter(test_df, name_1 != name_2)
-#	loop_df <- filter(test_df, i != name_2)
-	loop_df <- filter(test_df, i != name)
-#	print(loop_df$name)
-	write_csv2(loop_df,
-						 paste0("data/names-test/",
-						 			 i,
-						 			 ".csv"))
-}
-
-print(paste0("Wrote ", nrow(test_df), " files with ", nrow(test_df) - 1, " rows each"))
